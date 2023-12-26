119a9e4d86fd826fee1a8f3f2d6fedc02d9ae225
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2023-12-26 13:03:29 -0300

    Ajustado para a validação do form

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<{children: string, onClick: React.MouseEventHandler<HTMLButtonElement> | undefined, isValid: boolean}>`
     width: 100%;
     height: 42px;
     background-color: #81259D;
@@ -11,7 +11,12 @@ export const ButtonContainer = styled.button`
     border-radius: 21px;
 
     &:hover {
-        opacity: 0.6;
-        cursor:pointer;
+        opacity: ${({ isValid }) => (isValid ? 0.6 : 1)};
+        cursor: ${({ isValid }) => (isValid ? 'pointer' : 'not-allowed')};
+    }
+    &:disabled {
+        background-color: #ccc;
+        border: 1px solid #ccc;
+        cursor: not-allowed;
     }
 `
